f7a71662bb97372e1986f5835150e2be8b046527
Yurii Rysyn <zerocool_cs@ukr.net>
2020-07-30 21:24:43 +0300

    Updates

---
+++ precache-manifest.294bf929d9e2e4ae98359c62aef5b6d3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a450baca6f3cc509d5269085799fe6ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b3cec102a1bfdd019da2",
+    "url": "./static/css/main.bb34ef98.chunk.css"
+  },
+  {
+    "revision": "2dccce6a023ac7bdcd3d",
+    "url": "./static/js/2.75343c50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.75343c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cec102a1bfdd019da2",
+    "url": "./static/js/main.1925091c.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  },
+  {
+    "revision": "8cb5d82b8340c15cb75317278eb0f30b",
+    "url": "./static/media/Accessories.8cb5d82b.svg"
+  },
+  {
+    "revision": "71880ef8a6b3148aeeb6e76c154a702d",
+    "url": "./static/media/Favourites-Heart-Like.71880ef8.svg"
+  },
+  {
+    "revision": "49f665877b6b6081e14cef4569870268",
+    "url": "./static/media/LOGO.49f66587.svg"
+  },
+  {
+    "revision": "c71ff12d1719d058b0634a52acca3c1f",
+    "url": "./static/media/Mobile-phones.c71ff12d.svg"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  },
+  {
+    "revision": "032a50cf1345af9ac76324ff5b0e4e2d",
+    "url": "./static/media/basket.032a50cf.svg"
+  },
+  {
+    "revision": "5ca5c1f58aa3bcefb2bf88439be8fe51",
+    "url": "./static/media/btn-left.5ca5c1f5.svg"
+  },
+  {
+    "revision": "67be4a4a4d709745ad41a3578d953895",
+    "url": "./static/media/btn-right.67be4a4a.svg"
+  },
+  {
+    "revision": "6da4f5192e45db4de6412d3569a2a464",
+    "url": "./static/media/btn-up.6da4f519.svg"
+  },
+  {
+    "revision": "eb22d22b3e88739f4718d7be51171d9c",
+    "url": "./static/media/tablets.eb22d22b.svg"
+  }
+]);
